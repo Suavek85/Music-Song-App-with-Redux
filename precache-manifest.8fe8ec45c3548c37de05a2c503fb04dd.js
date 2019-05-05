4dfcb21d33c2d6e285018f7b670f17cc5f907b6f
SNiemiec <slawekniemiec@gmail.com>
2019-05-06 00:22:24 +0200

    Updates

---
+++ precache-manifest.8fe8ec45c3548c37de05a2c503fb04dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7223fc142c48ecbdfd7c01650d382fae",
+    "url": "/Music-Song-App-with-Redux/static/media/music-black-white-rotate.7223fc14.jpg"
+  },
+  {
+    "revision": "cc43da8d17ce226979b1",
+    "url": "/Music-Song-App-with-Redux/static/css/main.77cc6bc0.chunk.css"
+  },
+  {
+    "revision": "c7711ae08ebca60a824f",
+    "url": "/Music-Song-App-with-Redux/static/js/runtime~main.c6db68be.js"
+  },
+  {
+    "revision": "be5c6afa5ccb27cca826",
+    "url": "/Music-Song-App-with-Redux/static/js/2.90ee38af.chunk.js"
+  },
+  {
+    "revision": "1c419989fca3f7513882a00887881928",
+    "url": "/Music-Song-App-with-Redux/static/media/music-black-white.1c419989.jpg"
+  },
+  {
+    "revision": "cc43da8d17ce226979b1",
+    "url": "/Music-Song-App-with-Redux/static/js/main.bfe2f07e.chunk.js"
+  },
+  {
+    "revision": "2d2c23edfec4cc77a30f08f1030a7925",
+    "url": "/Music-Song-App-with-Redux/static/media/brazil.2d2c23ed.jpg"
+  },
+  {
+    "revision": "87e4a6a5322131eb472276b69be0093a",
+    "url": "/Music-Song-App-with-Redux/static/media/usa.87e4a6a5.jpg"
+  },
+  {
+    "revision": "a284017ceadb97386531040651ed6289",
+    "url": "/Music-Song-App-with-Redux/static/media/spain.a284017c.jpg"
+  },
+  {
+    "revision": "17e4dd243a8c8a43512ece62ce2d31bc",
+    "url": "/Music-Song-App-with-Redux/static/media/generic.17e4dd24.jpg"
+  },
+  {
+    "revision": "2481f339418d2f4583295a41a9d354cf",
+    "url": "/Music-Song-App-with-Redux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Music-Song-App-with-Redux/precache-manifest.8fe8ec45c3548c37de05a2c503fb04dd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Music-Song-App-with-Redux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
