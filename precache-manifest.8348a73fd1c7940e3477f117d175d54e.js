3adc6fa79352799e3f81eb6b3ad0a40beb1b8e1a
SNiemiec <slawekniemiec@gmail.com>
2019-05-24 09:49:21 +0200

    Updates

---
+++ precache-manifest.8348a73fd1c7940e3477f117d175d54e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7223fc142c48ecbdfd7c01650d382fae",
+    "url": "/Music-Song-App-with-Redux/static/media/music-black-white-rotate.7223fc14.jpg"
+  },
+  {
+    "revision": "35dfe508c7d3ca0c3ba0",
+    "url": "/Music-Song-App-with-Redux/static/css/main.dcc5e423.chunk.css"
+  },
+  {
+    "revision": "c7711ae08ebca60a824f",
+    "url": "/Music-Song-App-with-Redux/static/js/runtime~main.c6db68be.js"
+  },
+  {
+    "revision": "1813b4b1c6e3f7fa3c52",
+    "url": "/Music-Song-App-with-Redux/static/js/2.7e15d4e0.chunk.js"
+  },
+  {
+    "revision": "1c419989fca3f7513882a00887881928",
+    "url": "/Music-Song-App-with-Redux/static/media/music-black-white.1c419989.jpg"
+  },
+  {
+    "revision": "35dfe508c7d3ca0c3ba0",
+    "url": "/Music-Song-App-with-Redux/static/js/main.d2b3334b.chunk.js"
+  },
+  {
+    "revision": "2d2c23edfec4cc77a30f08f1030a7925",
+    "url": "/Music-Song-App-with-Redux/static/media/brazil.2d2c23ed.jpg"
+  },
+  {
+    "revision": "87e4a6a5322131eb472276b69be0093a",
+    "url": "/Music-Song-App-with-Redux/static/media/usa.87e4a6a5.jpg"
+  },
+  {
+    "revision": "a284017ceadb97386531040651ed6289",
+    "url": "/Music-Song-App-with-Redux/static/media/spain.a284017c.jpg"
+  },
+  {
+    "revision": "17e4dd243a8c8a43512ece62ce2d31bc",
+    "url": "/Music-Song-App-with-Redux/static/media/generic.17e4dd24.jpg"
+  },
+  {
+    "revision": "dfd03c29cf36304dc1d00baada1f1af7",
+    "url": "/Music-Song-App-with-Redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://suavek85.github.io/Music-Song-App-with-Redux/precache-manifest.7c69caa55157b741de941f4ee23bdf2e.js"
+  "/Music-Song-App-with-Redux/precache-manifest.8348a73fd1c7940e3477f117d175d54e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://suavek85.github.io/Music-Song-App-with-Redux/index.html", {
+workbox.routing.registerNavigationRoute("/Music-Song-App-with-Redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
